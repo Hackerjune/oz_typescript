de4a8fbe696a7b3bccbef8cb8bde8cf9f0827ee9
Hackerjun <psn_0125@naver.com>
2024-01-22 14:26:13 +0900

    배열

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("hi");
+let nums :number[] = [1, 2, 3, 4]
+
+//배열을 만들땐 변수 선언 후 배열 안 요소의 타입과 배열을 선언한다는 [] 기호까지 붙여준다
+
+let strs: string[] = ["1", "2"]
+
+nums.push(7)
+
+for (const str of strs) {
+        str.charAt 
+
+}
